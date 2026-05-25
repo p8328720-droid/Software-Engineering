3e795fc44df03aff6243de480346ef6a365d1ca8
allvaa <alvamailindo@gmail.com>
2026-05-25 09:56:44 +0700

    dockerize

---
+++ docker/entrypoint.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+chmod -R 775 /var/www/storage /var/www/bootstrap/cache 2>/dev/null
+chown -R www-data:www-data /var/www/storage /var/www/bootstrap/cache 2>/dev/null
+if [ ! -f .env ]; then
+
+    cp .env.example .env
+fi
+
+if ! grep -q "APP_KEY=base64" .env; then
+    php artisan key:generate
+fi
+
+# echo "Memeriksa & menginstal Composer dependencies..."
+# composer install --no-interaction --prefer-dist --optimize-autoloader
+
+echo "Menunggu transisi final database..."
+sleep 5
+until php artisan db:monitor > /dev/null 2>&1; do
+    echo "Database sedang inisialisasi/restart, mencoba lagi dalam 3 detik..."
+    sleep 3
+done
+
+echo "Database siap! Menjalankan migrasi dan seeding..."
+php artisan migrate:fresh --force --seed
+
+echo "Menyalakan PHP-FPM..."
+# Menggunakan exec agar PHP-FPM menjadi proses utama (PID 1) di container ini
+exec php-fpm
