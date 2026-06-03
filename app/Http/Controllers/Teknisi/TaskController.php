c94f596988c07aa63bc1d87a304a36a3bc338a6c
allvaa <alvamailindo@gmail.com>
2026-06-03 19:13:41 +0700

    fix: import model

--- app/Http/Controllers/Teknisi/TaskController.php
+++ app/Http/Controllers/Teknisi/TaskController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Teknisi;
 
 use App\Http\Controllers\Controller;
 use App\Models\Report;
+use App\Models\ReportStatus;
 use Illuminate\Http\Request;
 
 class TaskController extends Controller
